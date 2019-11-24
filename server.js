941f91efeb18abcfe5130c25192df9addf2db0f5
Gael Gohoungo <gohoungogael@gmail.com>
2019-11-24 06:59:23 -0500

    modified server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
-var port = process.env.PORT || 3000;
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,7 +12,9 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(express.static("app/public"));
 
-require("./app/routing/api-routes")(app);
-require("./app/routing/html-routes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+var port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log("Listening on port %s", port));
